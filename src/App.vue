c8767907b272283cf6d9f1f790cefbb82d762510
欧根亲王 <1191986715@qq.com>
2022-07-03 11:49:10 +0800

    '项目目录结构初始化'

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <div>
-      <span> </span>
-    </div>
+    <h2>app组件</h2>
     <router-view />
   </div>
 </template>
@@ -20,24 +14,5 @@ export default {
 </script>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
