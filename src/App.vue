c42041cdbf533a3eb23b30ceff276dd68a710d2d
欧根亲王 <1191986715@qq.com>
2022-07-03 11:17:44 +0800

    测试提交

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,20 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <div>
+      <span> </span>
+    </div>
+    <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
 
 <style lang="less">
 #app {
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 30000,
+      host: 'localhost'
+    }
+  },
+  lintOnSave: false
 })
